949e7264646b9f92b192871bf37649edfd1f14ad
husseniswd <husseniswedi2@gmail.com>
2025-01-27 02:15:44 -0500

    Created a toogle section for the hero mobile section

--- main.js
+++ main.js
@@ -13,3 +13,32 @@ header.addEventListener('click', (e) => {
         menuOpen.style.display = '';
     }
 });
+
+// The following code is for the hero section
+const heroBox = document.querySelector('.hero');
+const toggle = document.querySelector('.toggle-change');
+toggle.addEventListener('click', (e) =>{
+    console.log(toggle.childNodes);
+    if(e.target.id === "blue"){
+        heroBox.childNodes[1].scrollIntoView({behavior: "smooth"});
+        toggle.childNodes[1].style.backgroundColor = "#0083BB";
+        toggle.childNodes[3].style.backgroundColor = "#F0F0F0";
+    }
+    else if(e.target.id === "grey"){
+        heroBox.childNodes[3].scrollIntoView({behavior: "smooth"});
+        toggle.childNodes[3].style.backgroundColor = "#0083BB";
+        toggle.childNodes[1].style.backgroundColor = "#F0F0F0";
+    }
+});
+heroBox.addEventListener('scroll', (e) =>{
+    // Calculate the scroll depth as a percentage
+    const scrollDepth = (heroBox.scrollLeft / (heroBox.scrollWidth - heroBox.clientWidth)) * 100;
+    if(scrollDepth < 50){
+        toggle.childNodes[1].style.backgroundColor = "#0083BB";
+        toggle.childNodes[3].style.backgroundColor = "#F0F0F0";
+    }
+    else{
+        toggle.childNodes[3].style.backgroundColor = "#0083BB";
+        toggle.childNodes[1].style.backgroundColor = "#F0F0F0";
+    }
+});
